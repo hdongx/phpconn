5cce5c5ae824a34f5dcbf6f1605cff4f72bb96d1
hdongx <hdon.gx@gmail.com>
2022-03-01 16:42:09 -0600

    cnt3

--- connect.php
+++ connect.php
@@ -3,17 +3,19 @@
 $conexion = pg_connect("host=172.17.0.11 port=5432 dbname=sampledb user=sample password=sample");
 
 if($conexion) {
-	$result = pg_query($conexion, "SELECT * FROM accounts WHERE user_id = 1");
-	//var_dump(pg_fetch_all($result));
-	
-	
-	while ($row = pg_fetch_row($result)) {
-	echo "User_ID: $row[0] \t";
-	echo "Name: $row[1] \t";
-	echo "Mail: $row[3] \t";
-	echo "<br />\n";
 	
 	print_r("Conexión a la base de datos");
+	
+	$result = pg_query($conexion, "SELECT * FROM accounts WHERE user_id = 1");
+	//var_dump(pg_fetch_all($result));
+		
+	while ($row = pg_fetch_row($result)) {
+		echo "User_ID: $row[0] \t";
+		echo "Name: $row[1] \t";
+		echo "Mail: $row[3] \t";
+		echo "<br />\n";
+	}
+	
 	
 } else {
 	print_r("Sin conexión a la base de datos");
